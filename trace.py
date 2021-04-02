1783a9711fefdeae91637f7c64d7b0c4f42d4514
Gary Quaresima <garyquaresima@gmail.com>
2021-04-02 03:20:43 -0400

    separate functioning

--- trace.py
+++ trace.py
@@ -1,21 +1,17 @@
 import os
 import sys
 import numpy as np
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
-from config import conf
 
-from probe import probe
 import vtk
 from vtk.numpy_interface import dataset_adapter as dsa
 from vtk.vtkCommonDataModel import vtkDataSet
 from vtk.vtkCommonExecutionModel import vtkAlgorithmOutput
 
-
 def trace(IC, Field, debug=False):
     import types
     assert isinstance(Field, types.FunctionType)
 
-    sign = +1
+    sign = -1
 
     from scipy.integrate import odeint
 
@@ -43,7 +39,7 @@ def trace(IC, Field, debug=False):
             if debug:
                 print('i = ' + str(i))
             soln = odeint(dXds, X0, s_grid)
-            R = soln[:, 0]**2+soln[:, 1]**2 + soln[:, 2]**2
+            R = soln[:, 0]**2 + soln[:, 1]**2 + soln[:, 2]**2
             # define condition on the field line points
             # Find first location where soln steps out-of-bounds
             #tr = np.where( False == (R >= 1) & (soln[:,0] > -30.) & (np.abs(soln[:, 2]) < 20.) )        
@@ -81,14 +77,15 @@ def trace(IC, Field, debug=False):
 def interpolate_and_trace(IC, Field, Domain, debug=False):
     from scipy.interpolate import RegularGridInterpolator, NearestNDInterpolator
 
-    Space = np.array(Space)
     Field = np.array(Field)
+    Domain = np.array(Domain)
 
-    if Space.shape == (3,3):
+    if Domain.shape == (3,2) or Domain.shape == (2,3):
+        #TODO
         #xlims = ... TODO
 
-        from make_grid import make_axes
-        ax_list = make_axes(xlims,ylims,zlims,d)
+        #from make_grid import make_axes
+        #ax_list = make_axes(xlims,ylims,zlims,d)
 
         # https://stackoverflow.com/questions/21836067/interpolate-3d-volume-with-numpy-and-or-scipy
         Fx_interp = RegularGridInterpolator(tuple(ax_list), Field[0, :,:,:])
@@ -99,14 +96,14 @@ def interpolate_and_trace(IC, Field, Domain, debug=False):
             return np.array([Fx_interp(v)[0], Fy_interp(v)[0], Fz_interp(v)[0]])
 
     else:
-        assert(Space.shape == Field.shape)
-        if Space.shape[0]==3 and Space.shape[1]!=3:
-            Space = Space.transpose()
+        assert(Domain.shape == Field.shape)
+        if Domain.shape[0]==3 and Domain.shape[1]!=3:
+            Domain = Domain.transpose()
             Field = Field.transpose()
 
-        Fx = NearestNDInterpolator(Space,Field[:,0])
-        Fy = NearestNDInterpolator(Space,Field[:,0])
-        Fz = NearestNDInterpolator(Space,Field[:,0])
+        Fx_interp = NearestNDInterpolator(Domain,Field[:,0])
+        Fy_interp = NearestNDInterpolator(Domain,Field[:,1])
+        Fz_interp = NearestNDInterpolator(Domain,Field[:,2])
 
         def Fcallable(v):
             return np.array([Fx_interp(v)[0], Fy_interp(v)[0], Fz_interp(v)[0]])
@@ -114,8 +111,15 @@ def interpolate_and_trace(IC, Field, Domain, debug=False):
     return trace(IC, Fcallable, debug=debug)
 
 # see magnetosphere/misc/vtk/streamline_from_datafile_demo.py
-def trace_vtk(IC, vtk_object, debug=False, var='b'):
-    vtk_object.GetPointData().SetActiveVectors(var)
+def trace_vtk(IC, vtk_object, debug=False, var='b', celldata=True):
+    if celldata:
+        vtk_object.GetCellData().SetActiveVectors(var)
+        c2p = vtk.vtkCellDataToPointData()
+        c2p.SetInputDataObject(vtk_object)
+        tostreamer = c2p.GetOutputPort()
+    else:
+        vtk_object.GetPointData().SetActiveVectors(var)
+        tostreamer = vtk_object
 
     if IC.shape == (3,):
         IC = [IC]
@@ -127,12 +131,12 @@ def trace_vtk(IC, vtk_object, debug=False, var='b'):
         # Create source for streamtubes
         streamer = vtk.vtkStreamTracer()
 
-        if isinstance(vtk_object, vtkDataSet):
-            streamer.SetInputDataObject(vtk_object)
-        elif isinstance(vtk_object, vtkAlgorithmOutput):
-            streamer.SetInputConnection(vtk_object)
-            raise RuntimeWarning ('cannot at present select the apropriate vector field. '\
-                                  +  'will use whatever defaults.')
+        if isinstance(tostreamer, vtkDataSet):
+            streamer.SetInputDataObject(tostreamer)
+        elif isinstance(tostreamer, vtkAlgorithmOutput):
+            streamer.SetInputConnection(tostreamer)
+            #raise RuntimeWarning ('cannot at present select the apropriate vector field. '\
+            #                      +  'will use whatever defaults.')
         else:
             raise ValueError ('not a supported vtk object for streamlines')
 
@@ -144,8 +148,8 @@ def trace_vtk(IC, vtk_object, debug=False, var='b'):
         streamer.SetInitialIntegrationStep(0.01)
         #streamer.SetIntegrationDirection()
         #streamer.SetIntegrationDirectionToForward()
-        #streamer.SetIntegrationDirectionToBackward()
-        streamer.SetIntegrationDirectionToBoth()
+        streamer.SetIntegrationDirectionToBackward()
+        #streamer.SetIntegrationDirectionToBoth()
         streamer.SetIntegrator(rk)
         streamer.SetRotationScale(0.5)
         streamer.SetMaximumError(1.0e-8)
@@ -161,7 +165,7 @@ def trace_vtk(IC, vtk_object, debug=False, var='b'):
     return ret
 
 def trace_file(IC, filename, method='vtk', debug=False):
-    if not os.path.exists(fname): raise FileNotFoundError ('no file ' + fname)
+    if not os.path.exists(filename): raise FileNotFoundError ('no file ' + fname)
     ext = filename[-4:]
 
     if method == 'scipy':
@@ -169,12 +173,12 @@ def trace_file(IC, filename, method='vtk', debug=False):
             import spacepy.pybats.bats as bats
             data3d = bats.Bats2d(filename)
 
-            X = np.nan*np.empty((data3d['x'].shape,3), dtype=float) # makes dtype float64
+            X = np.nan*np.empty((data3d['x'].size,3), dtype=np.float32)
             X[:,0] = data3d['x']
             X[:,1] = data3d['y']
             X[:,2] = data3d['z']
 
-            B = np.nan*np.empty(X.shape, dtype=float) # makes dtype float64
+            B = np.nan*np.empty(X.shape, dtype=np.float32)
             B[:,0] = data3d['bx']
             B[:,1] = data3d['by']
             B[:,2] = data3d['bz']
@@ -192,14 +196,23 @@ def trace_file(IC, filename, method='vtk', debug=False):
                 reader = vtk.vtkGenericDataObjectReader()
             elif ext == '.vtu':
                 reader = vtk.vtkXMLUnstructuredGridReader() # https://stackoverflow.com/questions/54044958/reading-data-from-a-raw-vtk-vtu-file
-            reader.SetFileName(fname)
+            reader.SetFileName(filename)
+
             reader.Update() # forces loading the file into memory
             # get nessesary vtk info from Reader object
             reader_output = reader.GetOutput() #not the "New Pipelin"
+
             return trace_vtk(IC, reader_output, debug=debug)
         elif ext == '.out':
-            #todo: call julia script
             VTKfilename = filename[:-4]+'.vtk'
+
+            ## if not already existing, make vtk file from swmf .out file, to be used in tracing
+            if not os.path.exists(fname[:-4]+'.vtk'):
+                print('\n\ngenerating vtk file\n\n')
+                rswmf.swmf2vtk(fname[:-4])
+            else:
+                print('\n\nusing existing vtk file\n\n')
+
             return traceFile(VTKfilename, IC, method=method, debug=debug)
         elif ext == '.cdf':
             raise RuntimeWarning ('should it be allowed?') #!!!!
@@ -207,6 +220,6 @@ def trace_file(IC, filename, method='vtk', debug=False):
             raise ValueError
 
     elif method == 'kameleon':
-        if ext!='.cdf' : raise ValueError
-        #TODO: copy from elsewhere
+        if ext!='.cdf' : raise ValueError('need a CDF file, currently no way to make it')
+        #TODO: use kameleons field line tracer.
 
